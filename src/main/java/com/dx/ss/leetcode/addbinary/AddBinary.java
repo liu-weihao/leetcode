af453306afb10ed9aa201efbfc5b4b8b97791cae
460181594@qq.com <460181594@qq.com>
2019-04-24 18:29:09 +0800

    二进制求和

--- src/main/java/com/dx/ss/leetcode/addbinary/AddBinary.java
+++ src/main/java/com/dx/ss/leetcode/addbinary/AddBinary.java
@@ -5,20 +5,52 @@ public class AddBinary {
     public String addBinary(String a, String b) {
 
         String result = "";
+        int diff = a.length() - b.length();
+        if (diff > 0) {
+            while (diff > 0) {
+                b = "0" + b;
+                diff--;
+            }
+        } else {
+            while (diff < 0) {
+                a = "0" + a;
+                diff++;
+            }
+        }
         char[] as = a.toCharArray();
         char[] bs = b.toCharArray();
         int plus = 0; //进位
-        int i = as.length - 1, j = bs.length - 1;
-        while (i > 0 && j > 0) {
-            if (as[i] != bs[j]) {
-                result = "1" + result;
-                continue;
-            }
-            result = "0" + result;
-            if (as[i] != 0) {
-                plus = 1;
+        int index = a.length();
+        while (index > 0) {
+            index--;
+            if (as[index] != bs[index]) {
+                if (plus > 0) {
+                    result = "0" + result;
+                } else {
+                    result = "1" + result;
+                }
+            } else {
+                if (plus > 0) {
+                    plus--;
+                    result = "1" + result;
+                } else {
+                    result = "0" + result;
+                }
+                if (as[index] == '1') {
+                    plus++;
+                }
             }
         }
+        while (plus > 0) {
+            result = "1" + result;
+            plus--;
+        }
         return result;
+    }
+
+    public static void main(String[] args) {
+        AddBinary addBinary = new AddBinary();
+        String result = addBinary.addBinary("10111", "101");
+        System.out.println(result);
     }
 }
