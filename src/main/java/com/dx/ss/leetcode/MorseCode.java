8660da17974466c7f5d11ea50bc8d989f4515e9c
460181594@qq.com <460181594@qq.com>
2019-04-27 16:30:22 +0800

    Unique Morse Code Words

---
+++ src/main/java/com/dx/ss/leetcode/MorseCode.java
@@ -0,0 +1,29 @@
+package com.dx.ss.leetcode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class MorseCode {
+
+    public int uniqueMorseRepresentations(String[] words) {
+        if (words == null || words.length == 0) return 0;
+        if (words.length == 1) return 1;
+        String[] dic = new String[]{".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."};
+        Set<String> codeSet = new HashSet<>();
+        for (String word : words) {
+            char[] chars = word.toCharArray();
+            StringBuilder builder = new StringBuilder(chars.length);
+            for (char c : chars) {
+                builder.append(dic[97 - c]);
+            }
+            codeSet.add(builder.toString());
+        }
+        return codeSet.size();
+    }
+    public static void main(String[] args){
+        String[] words = new String[]{"gin", "zen", "gig", "msg"};
+        MorseCode morseCode = new MorseCode();
+        int i = morseCode.uniqueMorseRepresentations(words);
+        System.out.println(i);
+    }
+}
